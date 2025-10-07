3f0587eedc56973dcfb4cd7b695cc47f726070ca
Carlos <carlosiilva66@gmail.com>
2025-10-07 02:43:36 -0300

    docs: adicionar ROADMAP, REFERENCES e CHANGELOG  iniciais

--- Java/capitulo_01/Ex01.java
+++ Java/capitulo_01/Ex01.java
@@ -4,17 +4,6 @@ Package java.util.Scanner;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite o primeiro número: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Digite o segundo número: ");
-        int num2 = scanner.nextInt();
-
-        int soma = num1 + num2;
-        System.out.println("A soma dos dois números é: " + soma);
-
-        scanner.close();
+       system.out.println("Digite seu nome:");
     }
 }
